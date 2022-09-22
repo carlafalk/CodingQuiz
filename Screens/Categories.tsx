51b97e3af5d6e34d91c52f084bc9c1e4d032ab1e
Carl Åfalk <carl.afalk@hotmail.se>
2022-09-22 15:20:03 +0200

    Basic navigation structure for screens

---
+++ Screens/Categories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
---
+++ Screens/Settings.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>settings</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
+
+const styles = StyleSheet.create({});
