f5cd9f9a11eb03ffdd7b224b402d75e71c358792
Carl Åfalk <carl.afalk@hotmail.se>
2022-09-23 01:46:04 +0200

    added minor styling to home and app

--- App.tsx
+++ App.tsx
@@ -41,7 +41,7 @@ export default function App() {
 
   return (
     <NavigationContainer onReady={onLayoutRootView}>
-      <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false, animation: "fade" }}>
         <RootStack.Screen name="Home" component={HomeScreen} />
         <RootStack.Screen name="About" component={AboutScreen} />
         <RootStack.Screen name="Categories" component={CategoriesScreen} />
