889b119446e6b81f75bc578cbf4462f7ffe855c9
osci-the-orca <88oscste@skola.boras.se>
2022-09-23 18:21:41 +0200

    changed from styleSheet to styled components

--- Components/TimerBar.tsx
+++ Components/TimerBar.tsx
@@ -1,6 +1,6 @@
 import Constants from "expo-constants";
 import React, { useEffect, useState } from "react";
-import { Animated, StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
 import { colors } from "../Styles/Shared";
 
 const TimerBar = () => {
@@ -22,66 +22,76 @@ const TimerBar = () => {
   }, [timeLeft]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.progressBar}>
-        <Animated.View style={[StyleSheet.absoluteFill, { backgroundColor: "#8BED4F", borderRadius: 8, width: `${timeLeft}%` }]} />
-        <View style={styles.progressBarShine}></View>
-        <View style={styles.progressBarClock}>
-          <Text style={styles.timeLeftText}>{timeLeft / 10}</Text>
-        </View>
-      </View>
-    </View>
+    <TimeBarContainer>
+      <TimeBar>
+        <RemainingTimeBar width={timeLeft} />
+        <TimeBarShine />
+        <TimeBarClock>
+          <TimeBarTime>{timeLeft / 10}</TimeBarTime>
+        </TimeBarClock>
+      </TimeBar>
+    </TimeBarContainer>
   );
 };
 
 export default TimerBar;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: Constants.statusBarHeight,
-    padding: 1,
-  },
-  progressBar: {
-    justifyContent: "center",
-    flexDirection: "row",
-    height: 30,
-    width: "80%",
-    borderColor: "#000",
-    borderWidth: 2,
-    borderRadius: 10,
-    position: "relative",
-  },
-  progressBarShine: {
-    position: "absolute",
-    top: 1,
-    height: 10,
-    width: "98%",
-    backgroundColor: "#ffffff6e",
-    borderRadius: 10,
-    borderBottomLeftRadius: 4,
-    borderBottomRightRadius: 4,
-  },
-  progressBarClock: {
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    top: -15,
-    right: 5,
-    height: 60,
-    width: 60,
-    backgroundColor: colors.lightPurple,
-    borderColor: colors.mustard,
-    borderWidth: 5,
-    borderRadius: 50,
-    elevation: 5,
-  },
-  timeLeftText: {
-    fontFamily: "ShareTechMono",
-    fontSize: 20,
-    color: colors.lightGreen,
-  },
-});
+const TimeBarContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: ${Constants.statusBarHeight}px;
+  padding: 1px;
+  border: 1px solid black;
+`;
+
+const TimeBar = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  height: 30px;
+  width: 80%;
+  border-radius: 10px;
+  border: 2px solid black;
+`;
+
+const RemainingTimeBar = styled.View<{ width: number }>`
+  background-color: ${colors.lightGreen};
+  border-radius: 8px;
+  width: ${(props) => props.width}%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+`;
+
+const TimeBarShine = styled.View`
+  top: 1px;
+  height: 10px;
+  width: 98%;
+  background-color: #ffffff6e;
+  border-radius: 10px;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+`;
+
+const TimeBarClock = styled.View`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  top: -15px;
+  right: 5px;
+  height: 60px;
+  width: 60px;
+  background-color: ${colors.lightPurple};
+  border: 5px solid ${colors.mustard};
+  border-radius: 50px;
+  elevation: 8;
+`;
+
+const TimeBarTime = styled.Text`
+  font-family: "ShareTechMono";
+  font-size: 20px;
+  color: ${colors.lightGreen};
+`;
