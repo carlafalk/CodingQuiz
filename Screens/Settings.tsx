e1673cb13ec61682ef5791bf69d088631f9bb527
4l3x <alexander.auermaa@gmail.com>
2022-09-23 10:57:00 +0200

    Mapping settings based on settingsData

--- Screens/Settings.tsx
+++ Screens/Settings.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import SettingItem from "../Components/Settings/SettingItem";
+import settingsData from "../data/settingsData";
+import { LgText, XlText } from "../Styles/texts";
+import { Container, Divider } from "../Styles/views";
 
 const SettingsScreen = () => {
+  const categories = settingsData.filter((x, i) => settingsData.findIndex((y) => x.category === y.category) === i).map((x) => x.category);
+
   return (
-    <View>
-      <Text>settings</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <XlText>Settings</XlText>
+      </View>
+      <Divider />
+      {categories.map((category) => (
+        <Container key={category}>
+          <LgText>{category}</LgText>
+          {settingsData
+            .filter((setting) => setting.category === category)
+            .map((item) => (
+              <SettingItem key={item.title} item={item} />
+            ))}
+          <Divider />
+        </Container>
+      ))}
+    </ScrollView>
   );
 };
 
 export default SettingsScreen;
-
-const styles = StyleSheet.create({});
