77146e1c9b2aee4ffcd6fef77d882a30e2ece637
osci-the-orca <88oscste@skola.boras.se>
2022-09-25 14:49:31 +0200

    functionality to mute sound on buttons, and some namechaning for clarity

--- Contexts/SoundContext.tsx
+++ Contexts/SoundContext.tsx
@@ -1,18 +1,25 @@
 import { Audio, AVPlaybackSource, AVPlaybackStatus } from "expo-av";
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import AllSounds, { SoundObject } from "../data/soundData";
 
 interface SoundContextValue {
+  allSounds: SoundObject[];
   playSound: (path: AVPlaybackSource) => void;
-  toggleSound: () => void;
+  playButtonEffect: () => void;
+  toggleMuteMusic: () => void;
+  toggleMuteButtonSound: () => void;
 }
 
 const SoundContext = createContext<SoundContextValue>({
+  allSounds: [],
   playSound: () => {
     console.warn("this is default provider");
   },
-  toggleSound: () => {
+  playButtonEffect: () => console.warn("this is default provider"),
+  toggleMuteMusic: () => {
     console.warn("this is default provider");
   },
+  toggleMuteButtonSound: () => console.warn("this is default provider"),
 });
 
 interface Props {
@@ -20,36 +27,78 @@ interface Props {
 }
 
 function SoundProvider({ children }: Props) {
-  const [sound, setSound] = useState<Audio.Sound>();
+  const [allSounds, setAllSounds] = useState<SoundObject[]>(AllSounds);
+  const [music, setMusic] = useState<Audio.Sound>();
+  const [buttonEffect, setButtonEffect] = useState<Audio.Sound>();
   const [soundStatus, setSoundStatus] = useState<AVPlaybackStatus>();
-  const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [isMusicMuted, setIsMusicMuted] = useState<boolean>(false);
+  const [isButtonSoundMuted, setIsButtonSoundMuted] = useState<boolean>(false);
 
   useEffect(() => {
-    return sound
+    console.log("LOADING ALL SOUNDS");
+    LoadAllSounds();
+  }, []);
+
+  useEffect(() => {
+    return music
       ? () => {
-          sound.unloadAsync();
-          console.log("Unloading Sound ");
+          music.unloadAsync();
         }
       : undefined;
-  }, [sound]);
+  }, [music]);
+
+  useEffect(() => {
+    return buttonEffect
+      ? () => {
+          buttonEffect.unloadAsync();
+        }
+      : undefined;
+  }, [buttonEffect]);
 
   const playSound = async (filePath: AVPlaybackSource) => {
-    if (!isMuted) {
+    if (!isMusicMuted) {
       const { sound, status } = await Audio.Sound.createAsync(filePath);
 
-      setSound(sound);
+      setMusic(sound);
 
       await sound.playAsync();
     }
   };
 
-  const toggleSound = async () => {
-    //TODO: Stop sound
-    !isMuted ? sound && setSoundStatus(await sound.setIsMutedAsync(true)) : sound && setSoundStatus(await sound.setIsMutedAsync(false));
-    setIsMuted(!isMuted);
+  const LoadAllSounds = async () => {
+    //TODO: find some way to load all sounds at once? :O
+    // const { sound, status } = await Audio.Sound.createAsync(allSounds[1].sound);
+    // setButtonEffect(sound);
   };
 
-  return <SoundContext.Provider value={{ playSound, toggleSound }}>{children}</SoundContext.Provider>;
+  const playButtonEffect = async () => {
+    // TODO play sound on buttonPress
+
+    if (!isButtonSoundMuted) {
+      const { sound, status } = await Audio.Sound.createAsync(allSounds[1].sound);
+      await sound.playAsync();
+      setButtonEffect(sound);
+    }
+  };
+
+  const toggleMuteMusic = async () => {
+    //TODO: Stop sound
+    !isMusicMuted ? music && setSoundStatus(await music.setIsMutedAsync(true)) : music && setSoundStatus(await music.setIsMutedAsync(false));
+    setIsMusicMuted(!isMusicMuted);
+  };
+
+  const toggleMuteButtonSound = async () => {
+    !isButtonSoundMuted
+      ? buttonEffect && setSoundStatus(await buttonEffect.setIsMutedAsync(true))
+      : buttonEffect && setSoundStatus(await buttonEffect.setIsMutedAsync(false));
+    setIsButtonSoundMuted(!isButtonSoundMuted);
+  };
+
+  return (
+    <SoundContext.Provider value={{ allSounds, playSound, playButtonEffect, toggleMuteMusic, toggleMuteButtonSound }}>
+      {children}
+    </SoundContext.Provider>
+  );
 }
 
 export const useSound = () => useContext(SoundContext);
