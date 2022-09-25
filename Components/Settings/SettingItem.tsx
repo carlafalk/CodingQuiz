aaae3e80570501ed15a3fd3ccb754b48f5f8b4b5
4l3x <alexander.auermaa@gmail.com>
2022-09-25 16:41:45 +0200

    files updated for theme coloring

--- Components/Settings/SettingItem.tsx
+++ Components/Settings/SettingItem.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Switch, View } from "react-native";
+import { useTheme } from "../../contexts/ThemeContext";
 import { SettingModel } from "../../models/SettingModel";
 import { colors } from "../../Styles/Shared";
 import { MdText, SmText } from "../../Styles/texts";
@@ -7,16 +8,21 @@ import { FlexBox } from "../../Styles/views";
 
 interface Props {
   item: SettingModel;
+  handleToggle: () => void;
 }
-const SettingItem = ({ item }: Props) => {
+const SettingItem = ({ item, handleToggle }: Props) => {
   const [isEnabled, setIsEnabled] = useState(item.isEnabled);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const { themeColors } = useTheme();
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => !previousState);
+    handleToggle();
+  };
 
   return (
     <FlexBox>
       <View>
-        <MdText>{item.title}</MdText>
-        <SmText>{item.desc}</SmText>
+        <MdText style={{color: themeColors.commons.white}}>{item.title}</MdText>
+        <SmText style={{color: themeColors.commons.white}}>{item.desc}</SmText>
       </View>
       <View>
         <Switch
