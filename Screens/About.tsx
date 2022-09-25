e18ee3facd5b7b9f27cad34c1c9aaa52a7c8c52f
Tommy Zarén <85tomzar@skola.boras.se>
2022-09-26 00:40:06 +0200

    added link to to github

--- Screens/About.tsx
+++ Screens/About.tsx
@@ -1,4 +1,6 @@
+import { AntDesign } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import styled from "styled-components/native";
 import { RootStackParams } from "../App";
@@ -15,7 +17,16 @@ const AboutScreen = ({ navigation }: Props) => {
       <TopSection title="about" onPress={() => navigation.goBack()} />
       <Logo topMargin={-50} size="medium" />
       <About>
-        <Header>About</Header>
+        <HeaderContainer>
+          <Header>About</Header>
+          <AntDesign
+            name="github"
+            size={30}
+            color="white"
+            onPress={async () => await WebBrowser.openBrowserAsync("https://github.com/carlafalk/CodingQuiz")}
+            style={{ position: "absolute", right: 0 }}
+          />
+        </HeaderContainer>
         <Description>School project for SUVNET21</Description>
         <Description style={{ marginBottom: 40 }}>Written in React Native</Description>
         <Creators>Made by</Creators>
@@ -30,6 +41,12 @@ const AboutScreen = ({ navigation }: Props) => {
 
 export default AboutScreen;
 
+const HeaderContainer = styled.View`
+  width: 100%;
+  justify-content: center;
+  flex-direction: row;
+`;
+
 const About = styled.View`
   background-color: ${colors.lightPurple};
   margin: 0 21px;
@@ -40,6 +57,7 @@ const About = styled.View`
 `;
 
 const Header = styled.Text`
+  flex-direction: row;
   font-size: 30px;
   font-family: ShareTechMono;
   color: ${colors.commons.white};
