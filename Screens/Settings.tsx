1c5f8de33f4ba368197e8dbeac458e3a1dac6ded
osci-the-orca <88oscste@skola.boras.se>
2022-09-28 21:27:11 +0200

    added toggleHaptics to settingsScreen

--- Screens/Settings.tsx
+++ Screens/Settings.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import SettingItem from "../Components/Settings/SettingItem";
+import { useHaptics } from "../contexts/HapticsContext";
 import { useSound } from "../contexts/SoundContext";
 // import { useSettings } from "../contexts/SettingContext";
 import { useTheme } from "../contexts/ThemeContext";
@@ -15,6 +16,7 @@ const SettingsScreen = () => {
 
   const { themeColors, toggleTheme } = useTheme();
   const { toggleMuteMusic, toggleMuteButtonSound, setIsButtonSoundMuted, isButtonSoundMuted, isMusicMuted, setIsMusicMuted } = useSound();
+  const { setIsHapticsDisabled, isHapticsDisabled, toggleHapticsIsDisabled } = useHaptics();
   // const { toggleHapticSetting, toggleVibrationSetting, toggleMusicSetting, toggleEffectsSetting } = useSettings();
 
   function handleToggle(item: SettingModel, value: boolean) {
@@ -23,18 +25,20 @@ const SettingsScreen = () => {
     }
     if (item.title === "Music") {
       toggleMuteMusic();
-      // setIsMusicMuted(!isMusicMuted);
-      // console.log(isMusicMuted);
     }
     if (item.title === "Effects") {
       toggleMuteButtonSound();
       setIsButtonSoundMuted(!isButtonSoundMuted);
+    }
+    if (item.title === "Haptics") {
+      toggleHapticsIsDisabled();
     }
   }
 
   const getToggleValues = (item: SettingModel) => {
     if (item.title === "Effects") return !isButtonSoundMuted;
     if (item.title === "Music") return !isMusicMuted;
+    if (item.title === "Haptics") return !isHapticsDisabled;
     return true;
   };
 
