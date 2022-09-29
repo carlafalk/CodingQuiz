ad525eab899ac5d2ea9ffa158fe32e99fc6428a3
4l3x <alexander.auermaa@gmail.com>
2022-09-29 12:41:36 +0200

    Moved data to avatarData

--- Components/User/AvatarCreator.tsx
+++ Components/User/AvatarCreator.tsx
@@ -3,115 +3,38 @@ import React, { useEffect, useState } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { AvatarProps, BigHead } from "react-native-bigheads";
 import { useTheme } from "../../contexts/ThemeContext";
-import { LgText } from "../../Styles/texts";
+import { avatarProps, avatarPropsArray, defaultAvatar, mapAvatarValues } from "../../data/avatarData";
+import { LgText, MdText } from "../../Styles/texts";
 
 const AvatarCreator = () => {
   const [selectedProp, setSelectedProp] = useState<keyof AvatarProps>("accessory");
-  let defaultAvatar: AvatarProps = {
-    accessory: "none",
-    bgColor: "purple",
-    bgShape: "squircle",
-    body: "chest",
-    clothing: "shirt",
-    clothingColor: "black",
-    eyebrows: "concerned",
-    eyes: "leftTwitch",
-    facialHair: "none",
-    graphic: "react",
-    hair: "balding",
-    hairColor: "blue",
-    hat: "none",
-    hatColor: "green",
-    lashes: false,
-    lipColor: "purple",
-    mouth: "vomitingRainbow",
-    showBackground: true,
-    size: 200,
-    skinTone: "light",
-  };
   const [avatar, setAvatar] = useState<AvatarProps>(defaultAvatar);
   const [selectedValue, setSelectedValue] = useState<any>(avatar[selectedProp]);
   const { themeColors } = useTheme();
+
   useEffect(() => {
     setAvatarPropValue();
+    console.log(avatar);
   }, [selectedValue]);
-  const avatarProps = {
-    accessory: ["none", "roundGlasses", "tinyGlasses", "shades", "faceMask", "hoopEarrings"],
-    bgColor: ["purple", "blue", "green", "red", "orange", "yellow", "turqoise", "pink"],
-    bgShape: ["squircle", "circle", "square"],
-    body: ["chest", "breasts"],
-    clothing: ["shirt", "naked", "dressShirt", "vneck", "tankTop", "dress", "denimJacket", "hoodie", "chequeredShirt", "chequeredShirtDark"],
-    clothingColor: ["blue", "green", "red", "black", "white"],
-    eyebrows: ["concerned", "raised", "leftLowered", "serious", "angry"],
-    eyes: [
-      "leftTwitch",
-      "normal",
-      "happy",
-      "content",
-      "squint",
-      "simple",
-      "dizzy",
-      "wink",
-      "hearts",
-      "crazy",
-      "cute",
-      "dollars",
-      "stars",
-      "cyborg",
-      "simplePatch",
-      "piratePatch",
-    ],
-    facialHair: ["none", "none2", "none3", "stubble", "mediumBeard", "goatee"],
-    graphic: ["none", "react", "redwood", "gatsby", "vue", "graphQL", "donut", "rainbow"],
-    hair: ["none", "balding", "long", "bun", "short", "pixie", "buzz", "afro", "bob", "mohawk"],
-    hairColor: ["blue", "orange", "pink", "black", "white", "blonde", "brown"],
-    hat: ["none", "none2", "none3", "none4", "none5", "beanie", "turban", "party", "hijab"],
-    hatColor: ["white", "blue", "green", "red", "black"],
-    // lashes: [true, false],
-    lipColor: ["purple", "green", "red", "turqoise", "pink"],
-    mouth: ["serious", "vomitingRainbow", "grin", "sad", "openSmile", "lips", "open", "tongue", "piercedTongue"],
-    // showBackground: [true, false],
-    // size: 200,
-    skinTone: ["red", "yellow", "black", "brown", "light", "dark"],
-  };
 
-  const anotherArray = [
-    "accessory",
-    "bgColor",
-    "bgShape",
-    "body",
-    "clothing",
-    "clothingColor",
-    "eyebrows",
-    "eyes",
-    "facialHair",
-    "graphic",
-    "hair",
-    "hairColor",
-    "hat",
-    "hatColor",
-    // "lashes",
-    "lipColor",
-    "mouth",
-    // "showBackground",
-    // "size",
-    "skinTone",
-  ];
+  useEffect(() => {
+    setSelectedValue(avatar[selectedProp]);
+  }, [selectedProp]);
 
   const incrementProp = () => {
-    const propIndex = anotherArray.indexOf(selectedProp);
-    if (propIndex === anotherArray.length - 1) {
-      setSelectedProp(anotherArray[0] as keyof AvatarProps);
+    const propIndex = avatarPropsArray.indexOf(selectedProp);
+    if (propIndex === avatarPropsArray.length - 1) {
+      setSelectedProp(avatarPropsArray[0] as keyof AvatarProps);
     } else {
-      setSelectedProp(anotherArray[propIndex + 1] as keyof AvatarProps);
+      setSelectedProp(avatarPropsArray[propIndex + 1] as keyof AvatarProps);
     }
   };
   const decrementProp = () => {
-    const propIndex = anotherArray.indexOf(selectedProp);
+    const propIndex = avatarPropsArray.indexOf(selectedProp);
     if (propIndex === 0) {
-      setSelectedProp(anotherArray[anotherArray.length - 1] as keyof AvatarProps);
+      setSelectedProp(avatarPropsArray[avatarPropsArray.length - 1] as keyof AvatarProps);
     } else {
-      setSelectedProp(anotherArray[propIndex - 1] as keyof AvatarProps);
+      setSelectedProp(avatarPropsArray[propIndex - 1] as keyof AvatarProps);
     }
   };
 
@@ -122,36 +45,92 @@ const AvatarCreator = () => {
   };
 
   const incrementIndexOfValuePropArray = () => {
-    const currentValue = selectedProp as keyof typeof avatarProps;
-    const currentPropValue = avatarProps[currentValue];
-    const propIndex = currentPropValue.indexOf(selectedValue as string);
-    setSelectedValue(currentPropValue[propIndex + 1]);
+    const currentProp = selectedProp as keyof typeof avatarProps;
+    const currentPropValueArray = avatarProps[currentProp];
+    const propIndex = currentPropValueArray.indexOf(selectedValue as string);
+    if (propIndex === currentPropValueArray.length - 1) {
+      setSelectedValue(currentPropValueArray[0]);
+    } else {
+      setSelectedValue(currentPropValueArray[propIndex + 1]);
+    }
+  };
+
+  const decrementIndexOfValuePropArray = () => {
+    const currentProp = selectedProp as keyof typeof avatarProps;
+    const currentPropValueArray = avatarProps[currentProp];
+    const propIndex = currentPropValueArray.indexOf(selectedValue as string);
+    if (propIndex === 0) {
+      setSelectedValue(currentPropValueArray[currentPropValueArray.length - 1]);
+    } else {
+      setSelectedValue(currentPropValueArray[propIndex - 1]);
+    }
   };
 
   return (
     <>
       <View style={{ alignItems: "center" }}>
-        <TouchableOpacity onPress={decrementProp}>
-          <Octicons name="chevron-up" size={24} color={themeColors.commons.white} />
-        </TouchableOpacity>
-
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <TouchableOpacity onPress={() => incrementIndexOfValuePropArray()}>
+        {/* <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => decrementIndexOfValuePropArray()}>
             <Octicons name="chevron-left" size={24} color={themeColors.commons.white} />
           </TouchableOpacity>
-
-          <BigHead {...avatar} />
-
-          {/* <BigHead bgColor="purple" bgShape="squircle" showBackground={true} size={200} /> */}
-          <TouchableOpacity onPress={() => setSelectedProp("hatColor")}>
+        <LgText style={{color: themeColors.commons.white}}>{mapAvatarValues[selectedProp.toString()]}</LgText>
+        <TouchableOpacity onPress={() => decrementIndexOfValuePropArray()}>
             <Octicons name="chevron-right" size={24} color={themeColors.commons.white} />
           </TouchableOpacity>
         </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => decrementIndexOfValuePropArray()}>
+            <Octicons name="chevron-left" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+        <LgText style={{color: themeColors.commons.white, padding: 20}}>{mapAvatarValues[selectedProp.toString()]}</LgText>
+        <TouchableOpacity onPress={() => decrementIndexOfValuePropArray()}>
+            <Octicons name="chevron-right" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+        </View> */}
 
-        <TouchableOpacity style={{ marginTop: 40 }} onPress={incrementProp}>
+        {/* <TouchableOpacity onPress={decrementProp}>
+          <Octicons name="chevron-up" size={24} color={themeColors.commons.white} />
+        </TouchableOpacity> */}
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          {/* <TouchableOpacity onPress={() => decrementIndexOfValuePropArray()}>
+            <Octicons name="chevron-left" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity> */}
+
+          <BigHead {...avatar} />
+          {/* <BigHead {...avatar} size={100} /> */}
+
+          {/* <BigHead bgColor="purple" bgShape="squircle" showBackground={true} size={200} /> */}
+          {/* <TouchableOpacity onPress={() => incrementIndexOfValuePropArray()}>
+            <Octicons name="chevron-right" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity> */}
+        </View>
+
+        {/* <TouchableOpacity style={{ marginTop: 40 }} onPress={incrementProp}>
           <Octicons name="chevron-down" size={24} color={themeColors.commons.white} />
-        </TouchableOpacity>
-        <LgText>{selectedProp}</LgText>
+        </TouchableOpacity> */}
+        <View style={{ borderRadius: 10, borderColor: '#eeeeee32', borderWidth: 1, padding: 10, flexDirection: 'row', alignItems: 'center', marginVertical: 10 }}>
+        <TouchableOpacity onPress={decrementProp}>
+            <Octicons name="chevron-left" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+          <View style={{ marginHorizontal: 20, width: '80%' }}>
+          <LgText style={{ color: themeColors.commons.white, textAlign: 'center' }}>{mapAvatarValues[selectedProp.toString()]}</LgText>
+          </View>
+          <TouchableOpacity onPress={incrementProp}>
+            <Octicons name="chevron-right" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+        </View>
+        <View style={{ borderRadius: 10, borderColor: '#eeeeee32', borderWidth: 1, padding: 10, flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity onPress={decrementIndexOfValuePropArray}>
+            <Octicons name="chevron-left" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+          <View style={{marginHorizontal: 20, width: '60%'}}>
+          <MdText style={{ color: themeColors.commons.white, textAlign: 'center' }}>{selectedValue}</MdText>
+          </View>
+          <TouchableOpacity onPress={incrementIndexOfValuePropArray}>
+            <Octicons name="chevron-right" size={24} color={themeColors.commons.white} />
+          </TouchableOpacity>
+      </View>
       </View>
     </>
   );
