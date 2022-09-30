c7dfb100646abb5597eb44870a13a11c85534ecc
4l3x <alexander.auermaa@gmail.com>
2022-09-30 13:06:11 +0200

    Model updated to use avatarprops

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,6 @@
-import { Image } from "react-native";
-import { BigHead } from "react-native-bigheads";
+import { AvatarProps } from "react-native-bigheads";
 
 export interface User {
     username: string,
-    avatar: typeof BigHead
+    avatar: AvatarProps
 }
