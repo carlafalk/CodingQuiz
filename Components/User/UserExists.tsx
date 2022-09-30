1a5b5c23a6061f495e2faa55b0ea2a351ed4d29f
osci-the-orca <88oscste@skola.boras.se>
2022-09-30 14:42:52 +0200

    added scrollview to view that contains usercards.

--- Components/User/UserExists.tsx
+++ Components/User/UserExists.tsx
@@ -2,6 +2,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
 import { BigHead } from "react-native-bigheads";
+import { ScrollView } from "react-native-gesture-handler";
 import styled from "styled-components/native";
 import { useTheme } from "../../contexts/ThemeContext";
 import { useUser } from "../../contexts/UserContext";
@@ -18,7 +19,15 @@ const UserExists = ({ handleClose, handleLogIn }: Props) => {
   const { themeColors } = useTheme();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 10, backgroundColor: "#00000090" }}>
-      <View style={{ width: "100%", alignItems: "center", backgroundColor: themeColors.deepPurple, borderRadius: 10 }}>
+      <View
+        style={{
+          width: "100%",
+          height: "85%",
+          alignItems: "center",
+          backgroundColor: themeColors.deepPurple,
+          borderRadius: 10,
+        }}
+      >
         <Header themeColors={themeColors}>
           <TouchableOpacity style={{ position: "absolute", top: "50%", right: 20 }} onPress={handleClose}>
             <MaterialIcons name="close" size={32} color={themeColors.commons.white} />
@@ -29,18 +38,20 @@ const UserExists = ({ handleClose, handleLogIn }: Props) => {
           <InfoText themeColors={themeColors}>
             {users.length > 1 ? "There's multiple users on your phone. Which would you like to log into?" : `Welcome ${users[0].username}!`}
           </InfoText>
-          {users.map((user) => (
-            <View key={user.username} style={{ padding: 5, alignItems: "center" }}>
-              <UserButton onPress={() => handleLogIn(user)} themeColors={themeColors}>
-                <ButtonEffect themeColors={themeColors} />
-                <BigHead {...user.avatar} size={75} />
-                <StyledText themeColors={themeColors} style={{ textAlign: "center" }}>
-                  {user.username}
-                </StyledText>
-                <MaterialIcons name="arrow-forward-ios" size={24} color={themeColors.commons.white} />
-              </UserButton>
-            </View>
-          ))}
+          <ScrollView style={{ height: "80%" }}>
+            {users.map((user) => (
+              <View key={user.username} style={{ padding: 5, alignItems: "center" }}>
+                <UserButton onPress={() => handleLogIn(user)} themeColors={themeColors}>
+                  <ButtonEffect themeColors={themeColors} />
+                  <BigHead {...user.avatar} size={75} />
+                  <StyledText themeColors={themeColors} style={{ textAlign: "center" }}>
+                    {user.username}
+                  </StyledText>
+                  <MaterialIcons name="arrow-forward-ios" size={24} color={themeColors.commons.white} />
+                </UserButton>
+              </View>
+            ))}
+          </ScrollView>
         </View>
       </View>
     </View>
