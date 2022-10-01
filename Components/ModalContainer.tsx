2f4c7fd44a1cbb64173ba39fd8f70fc267c71984
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-01 18:54:50 +0200

    created a generic modal design

---
+++ Components/ModalContainer.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components/native";
+import { useTheme } from "../contexts/ThemeContext";
+import { colorsModel } from "../models/ColorsModel";
+import ModalHeader from "./ModalHeader";
+
+interface Props {
+  height?: string;
+  children: ReactNode;
+  closeModal: () => void;
+  title: string;
+  headerColor?: string;
+  headerImg?: string;
+}
+
+const ModalContainer = ({ height, children, closeModal, title, headerColor, headerImg }: Props) => {
+  const { themeColors } = useTheme();
+  return (
+    <Container themeColors={themeColors} height={height}>
+      <ModalHeader title={title} closeModal={closeModal} headerColor={headerColor} headerImg={headerImg} />
+      {children}
+    </Container>
+  );
+};
+
+const Container = styled.View<{ height?: string; themeColors: colorsModel }>`
+  margin: 0 10px;
+  background-color: ${({ themeColors }) => themeColors.deepPurple};
+  border-radius: 10px;
+  overflow: hidden;
+  ${({ height }) => height && `height: ${height};`}
+`;
+
+export default ModalContainer;
---
+++ Components/ModalHeader.tsx
@@ -0,0 +1,45 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import React from "react";
+import styled from "styled-components/native";
+import { useTheme } from "../contexts/ThemeContext";
+import { colorsModel } from "../models/ColorsModel";
+import STMText from "./Texts/ShareTechMonoText";
+
+interface Props {
+  title: string;
+  closeModal: () => void;
+  headerColor?: string;
+  headerImg?: string;
+}
+
+const ModalHeader = ({ title, closeModal, headerColor, headerImg }: Props) => {
+  const { themeColors } = useTheme();
+
+  return (
+    <Header backgroundColor={headerColor} themeColors={themeColors}>
+      {headerImg && <CategoryImage source={headerImg} />}
+      <STMText size={25} center styles={{ flex: 1 }} uppercase>
+        {title}
+      </STMText>
+      <CloseButton onPress={closeModal}>
+        <MaterialIcons name="close" size={32} color={themeColors.commons.white} />
+      </CloseButton>
+    </Header>
+  );
+};
+
+const Header = styled.View<{ backgroundColor: string; themeColors: colorsModel }>`
+  background-color: ${({ backgroundColor, themeColors }) => (backgroundColor ? backgroundColor : themeColors.darkPurple)};
+  padding: 10px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const CloseButton = styled.TouchableOpacity``;
+
+const CategoryImage = styled.Image`
+  height: 40px;
+  width: 40px;
+`;
+
+export default ModalHeader;
