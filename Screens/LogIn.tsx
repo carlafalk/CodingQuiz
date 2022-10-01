203c5f7b715cf07478fe824b4a6374a7cb378843
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-01 18:57:36 +0200

    implemented generic modal in LogIn.tsx

--- Screens/LogIn.tsx
+++ Screens/LogIn.tsx
@@ -1,11 +1,12 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import { KeyboardAvoidingView, Modal, Platform, Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, Text } from "react-native";
 import styled from "styled-components/native";
 import { RootStackParams } from "../App";
 import Background from "../Components/Background";
 import HomeScreenButton from "../Components/Buttons/HomeScreenButton";
 import Logo from "../Components/Logo";
+import QuizModal from "../Components/QuizModal";
 import CreateUser from "../Components/User/CreateUser";
 import UserExists from "../Components/User/UserExists";
 import { useTheme } from "../contexts/ThemeContext";
@@ -51,39 +52,17 @@ const LogInScreen = ({ navigation }: Props) => {
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <Background>
-        <View>
-          <Modal
-            statusBarTranslucent={true}
-            animationType="fade"
-            transparent={true}
-            visible={createUserModalVisible}
-            onRequestClose={() => {
-              setCreateUserModalVisible(!createUserModalVisible);
-            }}
-          >
-            <CreateUser
-              handleClose={() => {
-                setCreateUserModalVisible(false);
-              }}
-            />
-          </Modal>
-          <Modal
-            statusBarTranslucent={true}
-            animationType="fade"
-            transparent={true}
-            visible={loginModalVisible}
-            onRequestClose={() => {
-              setLoginModalVisible(!loginModalVisible);
-            }}
-          >
-            <UserExists handleClose={() => setLoginModalVisible(false)} handleLogIn={handleLogIn} />
-          </Modal>
-        </View>
+        <QuizModal show={createUserModalVisible} closeModal={() => setCreateUserModalVisible(false)} title={"create user"}>
+          <CreateUser handleClose={() => setCreateUserModalVisible(false)} />
+        </QuizModal>
+
+        <QuizModal show={loginModalVisible} closeModal={() => setLoginModalVisible(false)} title={"choose user"} modalHeight={"85%"}>
+          <UserExists handleLogIn={handleLogIn} />
+        </QuizModal>
+
         <Logo size="large" topMargin={120} />
         <ButtonContainer>
-          {users.length > 0 && 
-          <HomeScreenButton onPress={() => setLoginModalVisible(true)} title="Log in" color={themeColors.lightGreen} />
-          }
+          {users.length > 0 && <HomeScreenButton onPress={() => setLoginModalVisible(true)} title="Log in" color={themeColors.lightGreen} />}
           <HomeScreenButton onPress={handleCreateUser} title="Create User" color={themeColors.mustard} />
           <Text style={{ textAlign: "center", color: themeColors.commons.white, paddingBottom: 20 }}>or</Text>
           <HomeScreenButton onPress={handlePlayAsGuest} title="Play as guest" color={themeColors.categories.css} />
