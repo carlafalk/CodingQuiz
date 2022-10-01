397993a35c8a37a51bb46178f501c51e60da50c8
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-01 18:57:14 +0200

    removed modal logic

--- Components/User/CreateUser.tsx
+++ Components/User/CreateUser.tsx
@@ -1,6 +1,4 @@
-import { MaterialIcons } from "@expo/vector-icons";
 import React, { useRef, useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
 import { AvatarProps } from "react-native-bigheads";
 import styled from "styled-components/native";
 import { useTheme } from "../../contexts/ThemeContext";
@@ -8,7 +6,6 @@ import { useUser } from "../../contexts/UserContext";
 import { defaultAvatar } from "../../data/avatarData";
 import { colorsModel } from "../../models/ColorsModel";
 import HomeScreenButton from "../Buttons/HomeScreenButton";
-import ModalBackground from "../ModalBackground";
 import STMText from "../Texts/ShareTechMonoText";
 import AvatarCreator from "./AvatarCreator";
 
@@ -32,60 +29,21 @@ const CreateUser = ({ handleClose }: Props) => {
   };
 
   return (
-    <ModalBackground styles={{ padding: 10 }}>
-      <ModalContainer themeColors={themeColors}>
-        <Header themeColors={themeColors}>
-          <CloseButton onPress={handleClose}>
-            <MaterialIcons name="close" size={32} color={themeColors.commons.white} />
-          </CloseButton>
-          <StyledText themeColors={themeColors}>Create user</StyledText>
-        </Header>
-        <ContentContainer>
-          <STMText size={14}>Pick a username</STMText>
-          <Input onChangeText={setUsername} themeColors={themeColors} value={username} />
-          <AvatarCreator avatarRef={avatarRef} />
-        </ContentContainer>
-        <HomeScreenButton title="create" color={themeColors.mustard} onPress={handleCreateUser} />
-      </ModalContainer>
-    </ModalBackground>
+    <>
+      <ContentContainer>
+        <STMText size={14}>Pick a username</STMText>
+        <Input onChangeText={setUsername} themeColors={themeColors} value={username} />
+        <AvatarCreator avatarRef={avatarRef} />
+      </ContentContainer>
+      <HomeScreenButton title="submit" color={themeColors.mustard} onPress={handleCreateUser} />
+    </>
   );
 };
 
 export default CreateUser;
 
-const ModalContainer = styled.View<{ themeColors: colorsModel }>`
-  width: 100%;
-  align-items: center;
-  background-color: ${({ themeColors }) => themeColors.deepPurple};
-  border-radius: 10px;
-`;
-
-const Header = styled.View<{
-  themeColors: colorsModel;
-}>`
-  background-color: ${(props) => props.themeColors.darkPurple};
-  align-items: center;
-  width: 100%;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  justify-content: center;
-  padding: 15px;
-`;
-
-const CloseButton = styled.TouchableOpacity`
-  position: absolute;
-  top: 50%;
-  right: 20px;
-`;
-
 const ContentContainer = styled.View`
   padding: 30px;
-`;
-
-const StyledText = styled.Text<{
-  themeColors: colorsModel;
-}>`
-  color: ${(props) => props.themeColors.commons.white};
 `;
 
 const Input = styled.TextInput<{
--- Components/User/UserExists.tsx
+++ Components/User/UserExists.tsx
@@ -1,6 +1,6 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import { BigHead } from "react-native-bigheads";
 import { ScrollView } from "react-native-gesture-handler";
 import styled from "styled-components/native";
@@ -10,50 +10,31 @@ import { colorsModel } from "../../models/ColorsModel";
 import { User } from "../../models/User";
 
 interface Props {
-  handleClose: () => void;
   handleLogIn: (user: User) => void;
 }
 
-const UserExists = ({ handleClose, handleLogIn }: Props) => {
+const UserExists = ({ handleLogIn }: Props) => {
   const { users } = useUser();
   const { themeColors } = useTheme();
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 10, backgroundColor: "#00000090" }}>
-      <View
-        style={{
-          width: "100%",
-          height: "85%",
-          alignItems: "center",
-          backgroundColor: themeColors.deepPurple,
-          borderRadius: 10,
-        }}
-      >
-        <Header themeColors={themeColors}>
-          <TouchableOpacity style={{ position: "absolute", top: "50%", right: 20 }} onPress={handleClose}>
-            <MaterialIcons name="close" size={32} color={themeColors.commons.white} />
-          </TouchableOpacity>
-          <StyledText themeColors={themeColors}>Choose user</StyledText>
-        </Header>
-        <View style={{ width: "100%", padding: 20 }}>
-          <InfoText themeColors={themeColors}>
-            {users.length > 1 ? "There's multiple users on your phone. Which would you like to log into?" : `Welcome ${users[0].username}!`}
-          </InfoText>
-          <ScrollView style={{ height: "80%" }}>
-            {users.map((user) => (
-              <View key={user.username} style={{ padding: 5, alignItems: "center" }}>
-                <UserButton onPress={() => handleLogIn(user)} themeColors={themeColors}>
-                  <ButtonEffect themeColors={themeColors} />
-                  <BigHead {...user.avatar} size={75} />
-                  <StyledText themeColors={themeColors} style={{ textAlign: "center" }}>
-                    {user.username}
-                  </StyledText>
-                  <MaterialIcons name="arrow-forward-ios" size={24} color={themeColors.commons.white} />
-                </UserButton>
-              </View>
-            ))}
-          </ScrollView>
-        </View>
-      </View>
+    <View style={{ width: "100%", padding: 20 }}>
+      <InfoText themeColors={themeColors}>
+        {users.length > 1 ? "There's multiple users on your phone. Which would you like to log into?" : `Welcome ${users[0].username}!`}
+      </InfoText>
+      <ScrollView style={{ height: "80%" }}>
+        {users.map((user) => (
+          <View key={user.username} style={{ padding: 5, alignItems: "center" }}>
+            <UserButton onPress={() => handleLogIn(user)} themeColors={themeColors}>
+              <ButtonEffect themeColors={themeColors} />
+              <BigHead {...user.avatar} size={75} />
+              <StyledText themeColors={themeColors} style={{ textAlign: "center" }}>
+                {user.username}
+              </StyledText>
+              <MaterialIcons name="arrow-forward-ios" size={24} color={themeColors.commons.white} />
+            </UserButton>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -87,18 +68,6 @@ const ButtonEffect = styled.View<{
   right: 0;
 `;
 
-const Header = styled.View<{
-  themeColors: colorsModel;
-}>`
-  background-color: ${(props) => props.themeColors.darkPurple};
-  align-items: center;
-  width: 100%;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  justify-content: center;
-  padding: 15px;
-`;
-
 const StyledText = styled.Text<{
   themeColors: colorsModel;
 }>`
@@ -107,9 +76,4 @@ const StyledText = styled.Text<{
 
 const InfoText = styled(StyledText)`
   padding: 10px;
-`;
-
-const AvatarImage = styled.Image`
-  height: 50px;
-  width: 50px;
 `;
