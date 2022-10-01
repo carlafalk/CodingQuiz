aa05fe447abbdba117a58e3bab1f07b8969d24c3
Tommy Zarén <85tomzar@skola.boras.se>
2022-10-01 13:26:19 +0200

    moved countdown to it's own component from screen

---
+++ Components/Game/CountDown.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, Easing } from "react-native";
+import styled from "styled-components/native";
+import { useSound } from "../../contexts/SoundContext";
+import STMText from "../Texts/ShareTechMonoText";
+import TopSection from "../TopSection";
+
+interface Props {
+  setCountingDown: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CountDown = ({ setCountingDown }: Props) => {
+  const animatedCounter = useRef(new Animated.Value(3)).current;
+  const [counter, setCounter] = useState("");
+  const { playGetReadySound } = useSound();
+
+  useEffect(() => {
+    playGetReadySound();
+  }, []);
+
+  const animate = () => {
+    return Animated.timing(animatedCounter, {
+      toValue: 0,
+      duration: 3000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(async () => {
+      await new Promise((f) => setTimeout(f, 1000));
+      setCountingDown(false);
+    });
+  };
+
+  useEffect(() => {
+    animate();
+    animatedCounter.addListener((value) => {
+      setCounter(Math.ceil(value.value).toString());
+    });
+    return () => {
+      animatedCounter.removeAllListeners();
+    };
+  }, []);
+
+  return (
+    // <Background>
+    <>
+      <TopSection title="get ready" />
+      <Animated.View style={{ marginTop: 100 }}>
+        <Counter size={150} center>
+          {counter}
+        </Counter>
+      </Animated.View>
+    </>
+    /* </Background> */
+  );
+};
+
+export default CountDown;
+
+const Counter = styled(STMText)`
+  margin: auto 0;
+`;
