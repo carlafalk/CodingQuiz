b93ef856cad2efae7ba5b7fb2c7c07c36cff4cd2
osci-the-orca <88oscste@skola.boras.se>
2022-10-03 14:24:31 +0200

    create default objetct gamesession

--- Screens/Game.tsx
+++ Screens/Game.tsx
@@ -1,20 +1,25 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { RootStackParams } from "../App";
 import Background from "../Components/Background";
 import CountDown from "../Components/Game/CountDown";
 import GameOver from "../Components/Game/GameOver";
 import RunGame from "../Components/Game/RunGame";
-import { AnswerInfo } from "../models/AnswerInfo";
+import { GameSessionModel } from "../models/GameSessionModel";
 
 type Props = NativeStackScreenProps<RootStackParams, "Game">;
 
 const GameScreen = ({ navigation, route }: Props) => {
   const [countingDown, setCountingDown] = useState(true);
   const [gameIsOver, setGameIsOver] = useState(false);
-  const [gameSession, setGameSession] = useState<AnswerInfo[]>([]);
-
-  useEffect(() => {}, [gameSession]);
+  const [gameSession, setGameSession] = useState<GameSessionModel>({
+    category: route.params.category,
+    fastestTime: -1,
+    slowestTime: -1,
+    avgTime: -1,
+    points: -1,
+    answers: [],
+  });
 
   function handlePressHome() {
     navigation.navigate("Home");
@@ -29,14 +34,22 @@ const GameScreen = ({ navigation, route }: Props) => {
       {countingDown ? (
         <CountDown setCountingDown={setCountingDown} />
       ) : !gameIsOver ? (
-        <RunGame category={route.params.category} gameIsOver={gameIsOver} setGameIsOver={setGameIsOver} setGameSession={setGameSession} />
-      ) : (
-        <GameOver
-          gameSession={gameSession}
+        <RunGame
           category={route.params.category}
-          handlePressHome={handlePressHome}
-          handlePressPlayAgain={handlePressPlayAgain}
+          gameIsOver={gameIsOver}
+          setGameIsOver={setGameIsOver}
+          setGameSession={setGameSession}
+          gameSession={gameSession}
         />
+      ) : (
+        gameSession.answers.length === 10 && (
+          <GameOver
+            gameSession={gameSession}
+            category={route.params.category}
+            handlePressHome={handlePressHome}
+            handlePressPlayAgain={handlePressPlayAgain}
+          />
+        )
       )}
     </Background>
   );
