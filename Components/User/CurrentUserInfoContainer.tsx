1be18caee3ad8689f975b9ade7281c059e3263f1
4l3x <alexander.auermaa@gmail.com>
2022-10-05 12:27:14 +0200

    button and modal for achievements added in userinfo

--- Components/User/CurrentUserInfoContainer.tsx
+++ Components/User/CurrentUserInfoContainer.tsx
@@ -1,12 +1,14 @@
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import React, { useRef, useState } from "react";
-import { AvatarProps, BigHead } from "react-native-bigheads";
+import { Pressable } from "react-native";
+import { AvatarProps, BigHead, theme } from "react-native-bigheads";
 import styled from "styled-components/native";
 import { useTheme } from "../../contexts/ThemeContext";
 import { useUser } from "../../contexts/UserContext";
 import { colorsModel } from "../../models/ColorsModel";
 import { User } from "../../models/User";
 import EditUserAvatar from "../Modal/ModalViews/EditUserAvatar";
+import UserAchievements from "../Modal/ModalViews/UserAchievements";
 import QuizModal from "../Modal/QuizModal";
 
 interface Props {
@@ -19,13 +21,18 @@ export default function CurrentUserInfoContainer({ user }: Props) {
   const [username, setUsername] = useState(currentUser?.username);
   const [isFocused, setIsFocused] = useState(false);
   const [editModalOpen, setEditModalOpen] = useState(false);
+  const [achievementModalOpen, setAchievementModalOpen] = useState(false);
   const avatarRef = useRef<AvatarProps>(user?.avatar as AvatarProps);
 
   const editUserHeaderImg = <MaterialIcons name="mode-edit" size={28} color="white" />;
+  const achievementHeaderImg = <FontAwesome name="trophy" size={28} color="white" />;
 
   return (
     <>
       <UserInfoContainer>
+        <AchievementButton themeColors={themeColors} onPress={() => setAchievementModalOpen(true)}>
+      <FontAwesome name="trophy" size={22} color={themeColors.categories.react} />
+      </AchievementButton>
         <BigHeadContainer>
           <BigHead {...user.avatar} size={150} onPress={() => setEditModalOpen(true)} />
           <FontAwesome name="paint-brush" size={16} color={themeColors.categories.react} style={{ position: "absolute", bottom: 5, right: 5 }} />
@@ -53,6 +60,16 @@ export default function CurrentUserInfoContainer({ user }: Props) {
         headerImg={editUserHeaderImg}
       >
         <EditUserAvatar avatarRef={avatarRef} user={user} setModalIsOpen={() => setEditModalOpen(false)} />
+      </QuizModal>
+      <QuizModal
+        modalHeight={"70%"}
+        show={achievementModalOpen}
+        closeModal={() => setAchievementModalOpen(false)}
+        title={"Achievements"}
+        headerColor={themeColors.darkPurple}
+        headerImg={achievementHeaderImg}
+      >
+        <UserAchievements />
       </QuizModal>
     </>
   );
@@ -87,3 +104,13 @@ const UsernameInput = styled.TextInput<{ themeColors: colorsModel }>`
   width: 100%;
   z-index: 999;
 `;
+
+const AchievementButton = styled.Pressable<{themeColors: colorsModel}>`
+  padding: 15px;
+  z-index: 999;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background-color: ${({ themeColors }) => themeColors.backgrounds.superLowOpacity};
+  border-radius: 15px;
+`
